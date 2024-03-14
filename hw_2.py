5dd52987bc94560556dfe0bfe1dfc806de6fc4d9
Pol24rus <pol_dv@mail.ru>
2024-03-14 22:36:07 +0700

    2024_03_14

--- hw_2.py
+++ hw_2.py
@@ -1,20 +1,35 @@
 a = int(input("Введите число номер один: "))
 b = int(input("Введите число номер два: "))
-c = input("Что вы хотите сделать - разделить (введите '/'), умножить (введите '*'), сложить (введите '+') или отнять "
-          "(введите '-') ?: ")
+c = input(
+    "Что вы хотите сделать - разделить (здесь и далее введите '/' или 'д'), умножить ('*' или 'у'), сложить ('+' или "
+    "'с') или отнять ('-' или 'р') ?: ")
 
-if c == "/":
-    result = int(a / b)
-elif c == "*":
-    result = int(a * b)
-elif c == "+":
-    result = int(a+b)
-elif c == "-":
-    result = int(a - b)
+try:
+    if c == "/" or c == "д":
+        result = (a / b)
+        res_2 = "деление"
+    elif c == "*" or c == "у":
+        result = int(a * b)
+        res_2 = "умножение"
+    elif c == "+" or c == "с":
+        result = int(a + b)
+        res_2 = "сложение"
+    elif c == "-" or "р":
+        result = int(a - b)
+        res_2 = "вычитание"
+except ZeroDivisionError:
+    result = "Ошибка"
+    res_2 = "разделить на ноль"
+# #
+except ValueError:
+    # except TypeError:
+    res_2 = "совершить действие с числом и строкой"
+    result = "Ошибка"
 
-# print(result)
-print("Результат : " + str(result))
-# try:
-#
-# except: ZeroDivisionError:
-#     print("На ноль делить нельзя")
+except Exception:
+    res_2 = "совершить неверное действие"
+    result = "Ошибка"
+else:
+    print('Всё хорошо.')
+
+print(f"С числами {a} и {b} вы решили выполнить {res_2}, результат = {result}")
